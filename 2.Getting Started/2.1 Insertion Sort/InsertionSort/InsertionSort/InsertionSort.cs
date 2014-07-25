12b99437f8d907f8a4dad3066c3497f7d34f1ae4
Stanimir <akastambata@gmail.com>
2014-07-25 20:11:38 +0300

    Adds Role of Algorithms in Computing and Getting started chapters.

---
+++ 2.Getting Started/2.1 Insertion Sort/InsertionSort/InsertionSort/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿namespace InsertionSort
+{
+    using System;
+    class InsertionSort
+    {
+        static void Main()
+        {
+            int[] array = { 5, 2, 4, 6, 1, 3 };
+
+            int currentElement = 0;
+            int previousIndex = 0;
+            for (int index = 1; index <= array.Length - 1; index++)
+            {
+                currentElement = array[index];
+                previousIndex = index - 1;
+
+                while (previousIndex >= 0 && array[previousIndex] > currentElement)
+                {
+                    array[previousIndex + 1] = array[previousIndex];
+                    previousIndex--;
+                }
+                array[previousIndex + 1] = currentElement;
+            }
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.WriteLine(array[index]);
+            }
+        }
+    }
+}
+
