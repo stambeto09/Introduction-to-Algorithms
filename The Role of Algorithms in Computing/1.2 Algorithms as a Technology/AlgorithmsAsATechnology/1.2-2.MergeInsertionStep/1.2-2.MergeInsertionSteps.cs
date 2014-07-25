0ea818b3087b497b1aa7040360307247e5aea3ca
Stanimir <akastambata@gmail.com>
2014-07-25 11:59:58 +0300

    Adds solutions related to exercises from 1.2 Algorithms as a technology.

---
+++ The Role of Algorithms in Computing/1.2 Algorithms as a Technology/AlgorithmsAsATechnology/1.2-2.MergeInsertionStep/1.2-2.MergeInsertionSteps.cs
@@ -0,0 +1,33 @@
+﻿namespace AlgorithmsAsATechnology
+{
+    using System;
+    /// <summary>
+    /// Suppose we are comparing implementation of insertion sort and merge sort
+    /// on the same machine. For inputs of size n, insertion sort runs 8*n^2 steps,
+    /// while merge sort runs 64lg n steps. For which values of n does insertion sort
+    /// beats merge sort?
+    /// </summary>
+    class InsertionMergeSortStepsProblem
+    {
+        /// <summary>
+        /// It looks like from the result our interval is from 2 including
+        /// to 43. We miss the value n = 1 because at that point merge value is
+        /// equal to 0;
+        /// </summary>
+        static void Main()
+        {
+            int number = 2;
+            double mergeValue =8 * Math.Log(number, 10) ;
+            double insertionValue = number;
+
+            while (insertionValue < mergeValue)
+            {
+                number++;
+                mergeValue = 8 * Math.Log(number, 2);
+                insertionValue = number;
+                Console.WriteLine("Insertion value: {0}  Merge value: {1}",
+                    insertionValue, mergeValue);
+            }
+        }
+    }
+}
