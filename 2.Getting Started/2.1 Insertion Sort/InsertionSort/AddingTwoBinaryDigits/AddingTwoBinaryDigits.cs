d3d9a279c281db9916277be9eae2c78ec35de042
Stanimir <akastambata@gmail.com>
2014-07-28 13:07:50 +0300

    Adds a solution for 2.1-4 - adding a two binary integer numbers.

---
+++ 2.Getting Started/2.1 Insertion Sort/InsertionSort/AddingTwoBinaryDigits/AddingTwoBinaryDigits.cs
@@ -0,0 +1,32 @@
+﻿namespace AddingTwoBinaryDigits
+{
+    using System;
+    /// <summary>
+    /// Consider the problem of adding two n-bit binary integers, stored in two
+    /// n-element array A and B. The sum of the two integers should be stored in binary form in an
+    /// (n+1)-element array C. State the problem formally and write pseudocode for adding the two integers.
+    /// </summary>
+    class AddingTwoBinaryDigits
+    {
+        static void Main()
+        {
+            int[] firstNumber = { 1, 1, 0, 0, 1, 0 };
+            int[] secondNumber = { 0, 1, 1, 1, 1, 0 };
+            int[] resultNumber = new int[firstNumber.Length + 1];
+            int carry = 0;
+
+            for (int index = firstNumber.Length - 1; index >= 0; index--)
+            {
+                resultNumber[index + 1] = (firstNumber[index] + secondNumber[index] + carry) % 2;
+                carry = (firstNumber[index] + secondNumber[index] + carry) / 2;
+                resultNumber[index] = carry & 1;
+            }
+
+            for (int index = 0; index <= resultNumber.Length - 1; index++)
+            {
+                Console.Write(resultNumber[index]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
