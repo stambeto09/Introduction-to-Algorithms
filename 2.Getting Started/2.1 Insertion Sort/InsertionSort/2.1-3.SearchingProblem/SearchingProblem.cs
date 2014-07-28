5766cb3aa1e22da7f0e81710e57c7b33b7225aee
Stanimir <akastambata@gmail.com>
2014-07-28 10:15:37 +0300

    Adds problem 2.1-3 solving the linear search problem.

---
+++ 2.Getting Started/2.1 Insertion Sort/InsertionSort/2.1-3.SearchingProblem/SearchingProblem.cs
@@ -0,0 +1,33 @@
+﻿namespace _2._1_3.SearchingProblem
+{
+    using System;
+    /// <summary>
+    /// Consider the searching problem:
+    /// Input: A sequence of n numbers A = {a1, a2,..., an} and a value v.
+    /// Output: An index i such that v = A[i] or the special value NIL if v does not appear in A.
+    /// Write pseudocode for linear search, which scans through the sequence, looking for v. Using loop invariant,
+    /// prove that your algorithm is correct. Make sure that your loop invariant fulfills the three
+    /// necessary properties.
+    /// </summary>
+    class SearchingProblem
+    {
+        static void Main()
+        {
+            int[] array = { 2, 10, 15, 3, 7, 14, 5, 9, 6 };
+            int value = 0;
+            Console.WriteLine(LinearSearch(array, value));
+        }
+
+        public static string LinearSearch(int[] array, int value)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] == value)
+                {
+                    return index.ToString();
+                }
+            }
+            return "NIL";
+        }
+    }
+}
