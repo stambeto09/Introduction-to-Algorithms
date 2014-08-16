14e3f29b969b9679192141a5e2ef28b40f9e23d0
Stanimir <akastambata@gmail.com>
2014-08-16 18:01:13 +0300

    Adds Hire assistant randomized problem.

---
+++ Algorithms/Algorithms/RandomHireAssistand/RandomHireAssistant.cs
@@ -0,0 +1,38 @@
+﻿namespace RandomizedAlgorithms
+{
+    using System;
+
+    class RandomHireAssistant
+    {
+        static void Main()
+        {
+            int numberOfCandidates = 10;
+            int[] candidates = new int[numberOfCandidates];
+
+            Random randomNumber = new Random();
+            for (int index = 0; index <= candidates.Length - 1; index++)
+            {
+                candidates[index] = randomNumber.Next(1, 100);
+            }
+
+            RandomizedHireAssistant(candidates);
+        }
+
+        private static void RandomizedHireAssistant(int[] candidates)
+        {
+            int bestCandidate = 0;
+            int numberOfHirings = 0;
+            for (int index = 0; index <candidates.Length - 1 ; index++)
+            {
+                if (candidates[index] > bestCandidate)
+                {
+                    bestCandidate = candidates[index];
+                    Console.WriteLine("New best candidate hired.");
+                    numberOfHirings++;
+                }
+            }
+
+            Console.WriteLine("There are {0} untill we find the best one.", numberOfHirings);
+        }
+    }
+}
