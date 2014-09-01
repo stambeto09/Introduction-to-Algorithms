f380b7c11b0ea2dc0308b114a9c676f7367fcfa8
Stanimir <akastambata@gmail.com>
2014-09-01 11:11:56 +0300

    Adds completed implementation of Priority queue + solution of 6.5-8 - implementation of Delete().

--- Algorithms/Algorithms/PriorityQueue/PriorityQueue.cs
+++ Algorithms/Algorithms/PriorityQueue/PriorityQueue.cs
@@ -16,9 +16,29 @@ namespace PriorityQueue
             this.tree = new Tree(new List<Node<int>>());
         }
 
-        public void Add(int value)
+        public void Insert(int value)
         {
-            this.tree.Nodes.Add(new Node<int>(value));
+            int minValue = int.MinValue;
+            this.tree.Nodes.Add(new Node<int>(minValue));
+            IncreaseKey(this.tree.HeapSize - 1, value);
+        }
+
+
+        /// <summary>
+        /// 6.5-8 The operation HEAP-DELETE(A, i) deletes the item in node i from heap A. Give
+        /// an implementation of HEAP-DELETE that runs in O(lgn) time for an n-element
+        /// max-heap.
+        /// </summary>
+        /// <param name="index">Holds the index of the element that we are going to delete</param>
+        public void Delete(int index)
+        {
+            if (this.tree.HeapSize <= 0)
+            {
+                throw new ArgumentException("Heap underflow");
+            }
+
+            // MaxHeapify(this.tree.Nodes, 0);
+            this.tree.Nodes.RemoveAt(index);
         }
 
         public int Maximum()
@@ -28,9 +48,15 @@ namespace PriorityQueue
 
         public int ExtractMax()
         {
+            if (this.tree.HeapSize <= 0)
+            {
+                throw new ArgumentException("Heap underflow");
+            }
             int maxValue = this.tree.Nodes[0].Value;
+            this.tree.Nodes[0].Value = this.tree.Nodes[this.tree.HeapSize - 1].Value;
             this.tree.Nodes.RemoveAt(0);
             MaxHeapify(this.tree.Nodes, 0);
+
             return maxValue;
         }
 
@@ -40,7 +66,7 @@ namespace PriorityQueue
             int rightIndex = Right(index);
             int largest = 0;
 
-            if (leftIndex < this.tree.HeapSize)
+            if (leftIndex < this.tree.HeapSize && nodes[leftIndex].Value > nodes[index].Value)
             {
                 largest = leftIndex;
             }
@@ -65,9 +91,18 @@ namespace PriorityQueue
             }
         }
 
-        public void IncreaseKey()
+        public void IncreaseKey(int index, int value)
         {
-            throw new NotImplementedException();
+
+            this.tree.Nodes[index].Value = value;
+
+            while (index > 0 && this.tree.Nodes[Parent(index)].Value < this.tree.Nodes[index].Value)
+            {
+                int thirdvalue = this.tree.Nodes[index].Value;
+                this.tree.Nodes[index].Value = this.tree.Nodes[Parent(index)].Value;
+                this.tree.Nodes[Parent(index)].Value = thirdvalue;
+                index = Parent(index);
+            }
         }
 
         private static int Parent(int index)
@@ -95,6 +130,18 @@ namespace PriorityQueue
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.Append("Queue result: ");
+            while (this.tree.HeapSize != 0)
+            {
+                output.Append(this.ExtractMax().ToString() + ", ");
+            }
+
+            return output.ToString().TrimEnd(new char[] { ',', ' ' });
         }
     }
 }
--- Algorithms/Algorithms/PriorityQueue/Program.cs
+++ Algorithms/Algorithms/PriorityQueue/Program.cs
@@ -11,15 +11,38 @@ namespace PriorityQueue
         static void Main()
         {
             PriorityQueue<int> queue = new PriorityQueue<int>();
-            queue.Add(10);
-            queue.Add(15);
-            queue.Add(25);
-            queue.Add(345);
+            queue.Insert(5);
+            queue.Insert(10);
+            queue.Insert(5);
+            queue.Insert(15);
+            queue.Insert(25);
+            queue.Insert(345);
 
             foreach (var item in queue)
             {
                 Console.WriteLine(item);
             }
+
+            Console.WriteLine("Increased key");
+            queue.IncreaseKey(0, 35);
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            queue.Insert(1000);
+            Console.WriteLine("Maximum: {0}", queue.Maximum());
+
+            Console.WriteLine();
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Delete");
+            queue.Delete(3);
+
+
         }
     }
 }
