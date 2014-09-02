1fa8e925df5362045be242ed4b4948ef875e4f98
Stanimir <akastambata@gmail.com>
2014-09-02 11:25:18 +0300

    Adds implementation of Queue using with Priority queue.

---
+++ Algorithms/Algorithms/6.5-7.QueueUsingPriorityQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _6._5_7.QueueUsingPriorityQueue
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            // Enqueue
+            queue.Insert(10);
+            queue.Insert(15);
+            queue.Insert(20);
+            queue.Insert(5);
+            queue.Insert(30);
+            queue.Insert(35);
+            queue.Insert(40);
+            queue.Insert(45);
+
+            // Dequeue
+            queue.ExtractMax();
+            queue.ExtractMax();
+            Console.WriteLine(queue.ExtractMax());
+            Console.WriteLine(queue.ToString());
+        }
+    }
+}
---
+++ Algorithms/Algorithms/6.5-7.QueueUsingPriorityQueue/Queue.cs
@@ -0,0 +1,37 @@
+﻿namespace _6._5_7.QueueUsingPriorityQueue
+{
+    using PriorityQueue;
+    using System;
+
+    /// <summary>
+    /// 6.5-7 Show how to implement a first-in, first out queue with a priority queue...
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class Queue<T> : PriorityQueue<T>
+    {
+        public Queue()
+        {
+        }
+
+        // Dequeue
+        public override int ExtractMax()
+        {
+            
+            if (base.tree.HeapSize <= 0)
+            {
+                throw new ArgumentException("Heap underflow");
+            }
+
+            int maxValue = base.tree.Nodes[0].Value;
+            base.tree.Nodes[0].Value = base.tree.Nodes[base.tree.HeapSize - 1].Value;
+            base.tree.Nodes.RemoveAt(0);
+
+            return maxValue;
+        }
+
+        public override void IncreaseKey(int index, int value)
+        {
+            base.tree.Nodes[index].Value = value;
+        }
+    }
+}
