c4425840b565272a889006b92658259cb48a14ed
Stanimir <akastambata@gmail.com>
2015-01-15 11:45:17 +0200

    Adds Tree implementation.

--- Algorithms/AlgorithmsSecondPart/TreeImplementation/Program.cs
+++ Algorithms/AlgorithmsSecondPart/TreeImplementation/Program.cs
@@ -1,16 +1,22 @@
 ﻿namespace TreeImplementation
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
     class Program
     {
         static void Main()
         {
-            Node<int> root = new Node<int>(10, null);
+            Tree<int> tree =
+                new Tree<int>(10,
+                    new Tree<int>(11,
+                        new Tree<int>(14),
+                        new Tree<int>(15)),
+                    new Tree<int>(12,
+                        new Tree<int>(16),
+                        new Tree<int>(17)),
+                    new Tree<int>(13,
+                        new Tree<int>(18),
+                        new Tree<int>(19)));
+
+            tree.PrintDFS();
         }
     }
 }
