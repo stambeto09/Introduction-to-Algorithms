630caf108528df760710af2a5d97b2976f6d3d87
Stanimir <akastambata@gmail.com>
2015-01-15 09:37:03 +0200

    Adds Tree implementation.

---
+++ Algorithms/AlgorithmsSecondPart/TreeImplementation/Node.cs
@@ -0,0 +1,54 @@
+﻿namespace TreeImplementation
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Node<T> : IComparable<Node<T>> where T : IComparable<T>
+    {
+        public T Data { get; set; }
+
+        public Node<T> Parent { get; set; }
+
+        public ICollection<Node<T>> Children { get; set; }
+
+        public Node(T data, Node<T> parent)
+        {
+            this.Data = data;
+            this.Children = new List<Node<T>>();
+        }
+
+        public bool isRoot()
+        {
+            if (this.Parent == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool isLeaf()
+        {
+            if (this.Children.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int CompareTo(Node<T> other)
+        {
+            if (this.Data.CompareTo(other.Data) < 0)
+            {
+                return -1;
+            }
+            else if (this.Data.CompareTo(other.Data) > 0)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Algorithms/AlgorithmsSecondPart/TreeImplementation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TreeImplementation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+            Node<int> root = new Node<int>(10, null);
+        }
+    }
+}
