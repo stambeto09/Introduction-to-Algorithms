42fd9bf84db857aa4c7fcb642b5c035ab1663ed2
stambeto09 <akastambata@gmail.com>
2015-03-18 16:10:08 +0200

    Adds counting sort implementation

---
+++ Algorithms/AlgorithmsSecondPart/CountingSort/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace CountingSort
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = new int[] { 6, 0, 2, 0, 1, 3, 4, 6, 1, 3, 2 };
+
+            int[] countingArray = new int[array.Max() + 1];
+
+            int[] sortedArray = new int[array.Length];
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                countingArray[array[index]]++;
+            }
+
+            int j = 0;
+            for (int index = 0; index < countingArray.Length; index++)
+            {
+                while (countingArray[index] > 0)
+                {
+                    sortedArray[j] = index;
+                    j++;
+                    countingArray[index]--;
+                }
+            }
+
+            PrintArray(sortedArray);
+        }
+
+        private static void PrintArray(int[] sortedArray)
+        {
+            for (int index = 0; index < sortedArray.Length; index++)
+            {
+                Console.WriteLine(sortedArray[index]);
+            }
+        }
+    }
+}
