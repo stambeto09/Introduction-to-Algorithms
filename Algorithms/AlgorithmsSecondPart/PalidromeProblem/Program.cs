fcf18d3f6c9b45b1d16ec060c6ff1933d2f552a5
stambeto09 <akastambata@gmail.com>
2015-03-26 13:31:52 +0200

    Adds two solutions of the palindrome problem

---
+++ Algorithms/AlgorithmsSecondPart/PalidromeProblem/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace PalindromeProblem
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] array =
+	        {
+	            "civic",
+	            "deified",
+	            "deleveled",
+	            "devoved",
+	            "dewed",
+	            "Hannah",
+	            "kayak",
+	            "level",
+	            "madam",
+	            "racecar",
+	            "radar",
+	            "redder",
+	            "refer",    
+	            "repaper",
+	            "reviver",
+	            "rotator",
+	            "rotor",
+	            "sagas",
+	            "solos",
+	            "sexes",
+	            "stats",
+	            "tenet",
+
+	            "Dot",
+	            "Net",
+	            "Perls",
+	            "Is",
+	            "Not",
+	            "A",
+	            "Palindrome",
+	            ""
+	        };
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item + " = " + IsPalindrome(item));
+            }
+
+            string word = "rotator";
+            Console.WriteLine(CraftedIsPalindrome(word));
+        }
+
+        private static bool IsPalindrome(string word)
+        {
+            int minIndex = 0;
+            int maxIndex = word.Length - 1;
+            while (true)
+            {
+                if (minIndex > maxIndex)
+                {
+                    return true;
+                }
+                char currentLeftChar = word[minIndex];
+                char currentRightChar = word[maxIndex];
+                if (char.ToLower(currentLeftChar) != char.ToLower(currentRightChar))
+                {
+                    return false;
+                }
+
+                minIndex++;
+                maxIndex--;
+            }
+        }
+
+        private static bool CraftedIsPalindrome(string word)
+        {
+            var reversed = new string(word.ToCharArray().Reverse().ToArray());
+            return word == reversed;
+        }
+    }
+}
